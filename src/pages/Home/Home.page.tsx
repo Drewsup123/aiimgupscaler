6f11b64487758006e1f931416c8014f4f2e4d7ff
Drew <drewsup123c@gmail.com>
2023-07-14 19:32:27 -0600

    bump

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,16 +1,45 @@
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import StyledDropzone from "../../components/Dropzone/StyledDropzone.component";
 import styles from "./Home.module.sass";
 import { convertBytesToSize } from "../../utils/conversion.util";
+import { generateId } from "../../utils/uuid.util";
+
+interface IFile {
+    file: File;
+    id: string;
+}
 
 const HomePage = () => {
-    const [files, setFiles] = useState<File[]>([]);
+    const [files, setFiles] = useState<IFile[]>([]);
+    const filesRef = useRef<IFile[]>([]);
+    if (files) {
+        filesRef.current = files;
+    }
 
-    const handleDrop = (droppedFiles: File[]) => {
-        setFiles([...files, ...droppedFiles]);
+    const handleDrop = useCallback(
+        (droppedFiles: File[]) => {
+            console.log("Dropped Files : ", droppedFiles);
+            const newFiles = droppedFiles.map((file) => ({
+                file,
+                id: generateId(),
+            }));
+            console.log("Old Files : ", files);
+            setFiles([...filesRef.current, ...newFiles]);
+        },
+        [files]
+    );
+
+    const startUpscale = (fileId: string) => {
+        const foundFile = files.find((f) => f.id === fileId);
+        console.log("Start upscale : ", foundFile);
     };
 
-    console.log("Dropped Files : ", files);
+    const handleRemove = (fileId: string) => {
+        const filteredFiles = [...files].filter((f) => f.id !== fileId);
+        setFiles(filteredFiles);
+    };
+
+    console.log("Files : ", files);
 
     return (
         <div className="route">
@@ -28,17 +57,27 @@ const HomePage = () => {
                 {files.map((file, index) => (
                     <div key={index} className={styles.filePreviewWrapper}>
                         <img
-                            src={URL.createObjectURL(file)}
-                            alt={file.name}
+                            src={URL.createObjectURL(file.file)}
+                            alt={file.file.name}
                             style={{ width: "100px", height: "100px" }}
                         />
                         <div className={styles.previewInfo}>
-                            <p>{file.name}</p>
-                            <p>{convertBytesToSize(file.size)}</p>
+                            <p>{file.file.name}</p>
+                            <p>{convertBytesToSize(file.file.size)}</p>
                         </div>
                         <div className={styles.actionsContainer}>
-                            <button className={styles.startBtn}>Start</button>
-                            <button className={styles.removeBtn}>Remove</button>
+                            <button
+                                className={styles.startBtn}
+                                onClick={() => startUpscale(file.id)}
+                            >
+                                Start
+                            </button>
+                            <button
+                                className={styles.removeBtn}
+                                onClick={() => handleRemove(file.id)}
+                            >
+                                Remove
+                            </button>
                         </div>
                     </div>
                 ))}
