8949093d767a6e6b73c340722bef76bed74e9e8f
Drew <drewsup123c@gmail.com>
2023-07-28 20:37:27 -0600

    bumpg

--- src/stripe/createCheckoutSession.ts
+++ src/stripe/createCheckoutSession.ts
@@ -7,13 +7,14 @@ export const createCheckoutSession = async (uid: string) => {
     const checkout_ref = doc(db, "checkout_sessions", uid);
     const user_ref = doc(db, "users", uid);
     const checkoutInfo = {
-        price: PREMIUM_ID,
-        success_url: window.location.origin,
-        cancel_url: window.location.origin,
+        checkout_sessions: {
+            price: PREMIUM_ID,
+            success_url: window.location.origin,
+            cancel_url: window.location.origin,
+        },
     };
     setDoc(checkout_ref, checkoutInfo, { merge: true });
     setDoc(user_ref, checkoutInfo, { merge: true });
-
     onSnapshot(checkout_ref, async (doc: any) => {
         const data = doc.data();
         console.log("Current Doc : ", data);
