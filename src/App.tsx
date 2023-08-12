b30af8c686ce841126d6a8e0cf8fe505098dc699
Drew <drewsup123c@gmail.com>
2023-08-11 18:45:13 -0600

    Adding footer to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header/header.component";
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/Home/Home.page";
-import TermsAndConditionsPage from "./pages/TermsAndConditions.page";
+import TermsAndConditionsPage from "./pages/TermsAndConditions/TermsAndConditions.page";
 import AccountPage from "./pages/Account/Account.page";
 import BgRemoverPage from "./pages/BgRemover/BgRemover.page";
 import InPaintingPage from "./pages/Inpainting/inpainting.page";
+import Footer from "./components/Global/Footer.component";
 
 function App() {
     return (
@@ -32,6 +33,7 @@ function App() {
                 <Route path="/inpainting" Component={InPaintingPage} />
                 <Route path="/account" Component={AccountPage} />
             </Routes>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Global/Footer.component.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="waves">
+                <div className="wave" id="wave1"></div>
+                <div className="wave" id="wave2"></div>
+                <div className="wave" id="wave3"></div>
+                <div className="wave" id="wave4"></div>
+            </div>
+            <ul className="menu">
+                <li className="menu__item">
+                    <Link className="menu__link" to="/">
+                        Home
+                    </Link>
+                </li>
+                <li className="menu__item">
+                    <Link className="menu__link" to="/terms">
+                        Terms of Use
+                    </Link>
+                </li>
+                <li className="menu__item">
+                    <Link className="menu__link" to="/privacy-policy">
+                        Privacy Policy
+                    </Link>
+                </li>
+                <li className="menu__item">
+                    <Link className="menu__link" to="/">
+                        Contact Us
+                    </Link>
+                </li>
+            </ul>
+            <p>&copy;2023 AI IMG UPSCALER | All Rights Reserved</p>
+        </footer>
+    );
+};
+
+export default Footer;
