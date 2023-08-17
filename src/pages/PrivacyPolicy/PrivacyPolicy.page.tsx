01a8b6027e8cd2540ddfcc806bc3320887b68eda
Drew <drewsup123c@gmail.com>
2023-08-16 19:05:16 -0600

    bump

--- src/pages/PrivacyPolicy/PrivacyPolicy.page.tsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.page.tsx
@@ -102,41 +102,14 @@ const PrivacyPolicy = () => {
                 shared.
             </p>
             {/* I STOPPED HERE DOUBLE CHECK THE ABOVE SECTION */}
-            <h3>5. Account Management</h3>
-            <p>
-                You must provide accurate and current information when creating
-                an account. Failure to comply may result in immediate
-                termination. You are responsible for the confidentiality and
-                security of your password and must notify us immediately of any
-                security breach.
-            </p>
-            <h3>6. Liability Limitations</h3>
-            <p>
-                As permitted by law, we are not liable for any indirect or
-                consequential losses. Our total liability is capped at the
-                amount you paid for the Services.
-            </p>
-            <h3>7. Third-Party Links</h3>
-            <p>
-                We have no control over third-party websites linked to our
-                Service and accept no responsibility for them. We advise you to
-                review their terms and privacy policies.
-            </p>
-            <h3>8. Termination Rights</h3>
-            <p>
-                You may stop using our Services at any time. We may also suspend
-                or terminate your access at our discretion. If inactive for 12
-                consecutive months, we may delete your account, giving prior
-                notice via email.
-            </p>
-            <h3>9. Changes to Terms</h3>
+            <h3>5. Changes to Terms</h3>
             <p>
                 These Terms may be updated periodically, with the current
                 version available on our website. Any significant reduction in
                 your rights will be communicated to you. By continuing to use
                 the Services, you accept the updated Terms.
             </p>
-            <h3>10. Contact Us</h3>
+            <h3>6. Contact Us</h3>
             <p>
                 [Provide contact information or directions for how users may get
                 in touch with you.]
