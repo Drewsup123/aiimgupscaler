5c8d7076c2eee3699335da984d17222518b719b8
Drew <drewsup123c@gmail.com>
2023-09-30 13:22:30 -0600

    bump for premium product id

--- src/stripe/createCheckoutSession.ts
+++ src/stripe/createCheckoutSession.ts
@@ -1,13 +1,13 @@
 import { doc, onSnapshot, collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase/client";
 import getStripe from "./initStripe";
-const PREMIUM_ID = "price_1NZ0sUFLfOdR6n29ElFnIWea";
+import { PREMIUM_PRODUCT_ID } from "../constants/keys.constant";
 
 export const createCheckoutSession = async (uid: string) => {
     const user_ref = doc(db, "users", uid);
     const checkout_sessions_ref = collection(user_ref, "checkout_sessions");
     const checkoutInfo = {
-        price: PREMIUM_ID,
+        price: PREMIUM_PRODUCT_ID,
         success_url: window.location.origin,
         cancel_url: window.location.origin,
     };
