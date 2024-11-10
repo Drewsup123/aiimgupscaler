64882235d3e8b1d62078cbdd4482f22d8ead2d96
Drew <drewsup123c@gmail.com>
2024-11-10 15:10:10 -0700

    bump

--- src/stripe/isUserPremium.ts
+++ src/stripe/isUserPremium.ts
@@ -2,7 +2,7 @@ import { auth } from "../firebase/client";
 
 const isUserPremium = async () => {
     const decoded_token = await auth.currentUser?.getIdTokenResult(true);
-    console.log("Checking : ", decoded_token)
+    console.log("Checking : ", decoded_token);
     if (decoded_token?.claims?.stripeRole === "premium") return true;
     return false;
 };
