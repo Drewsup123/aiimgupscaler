4547923e144c6321f56608cd2475f4228e1a0e13
Drew <drewsup123c@gmail.com>
2024-11-10 16:19:14 -0700

    bump

--- src/stripe/redirectToCustomerPortal.ts
+++ src/stripe/redirectToCustomerPortal.ts
@@ -4,7 +4,6 @@ export const redirectToCustomerPortal = async (uid?: string) => {
     const { data }: any = await createPortalLink({
         returnUrl: window.location.origin,
         locale: "auto",
-        // configuration: "bpc_1JSEAKHYgolSBA358VNoc2Hs",
     });
     if (!data.url) return;
     window.location.assign(data.url);
